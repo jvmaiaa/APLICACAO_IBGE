7c300e723f32e788c3b772ec2780e1b98d10ab0f
jvmaiaa <joaovictormaia482@gmail.com>
2024-02-10 04:01:02 -0300

    Remove o ResponseEntity do método deletar da entidade Address

--- src/main/java/com/example/pessoa/api/controller/AddressResource.java
+++ src/main/java/com/example/pessoa/api/controller/AddressResource.java
@@ -6,12 +6,12 @@ import com.example.pessoa.api.repository.AddressRepository;
 import com.example.pessoa.api.repository.PessoaRepository;
 import com.example.pessoa.api.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping(value = "/api/v2")
@@ -53,8 +53,7 @@ public class AddressResource {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteAddress(@PathVariable Long id) {
+    public void deleteAddress(@PathVariable Long id) {
         service.delete(id);
-        return ResponseEntity.noContent().build();
     }
 }
