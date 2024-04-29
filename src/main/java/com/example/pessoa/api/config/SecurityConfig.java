4392e6cfad71d21ea94d55b950912ad963e836fb
jvmaiaa <joaovictormaia482@gmail.com>
2024-04-29 00:35:14 -0300

    ✨feat: Utilzando autenticação via Filtro

--- src/main/java/com/example/pessoa/api/config/SecurityConfig.java
+++ src/main/java/com/example/pessoa/api/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,7 +21,9 @@ public class SecurityConfig {
     // classe q substitui configuração padrão do Spring Security (cadeia de filtros)
     @Bean
     public SecurityFilterChain securityFilterChain(
-            HttpSecurity http, SenhaMasterAuthenticationProvider senhaMasterAuthenticationProvider) throws Exception {
+            HttpSecurity http,
+            SenhaMasterAuthenticationProvider senhaMasterAuthenticationProvider,
+            CustomFilter customFilter) throws Exception {
         return http
                 .authorizeHttpRequests(customizer -> {
                     customizer.requestMatchers("/api/address").permitAll();
@@ -29,6 +32,7 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults()) // forma de se autenticar
                 .formLogin(Customizer.withDefaults()) // forma de se autenticar
                 .authenticationProvider(senhaMasterAuthenticationProvider) // forma de validar/autenticar o usuário
+                .addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
